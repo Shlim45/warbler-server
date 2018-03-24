8d9d6f891bbd9382d545bc78c32748e1a1db4d42
Shlim45 <jhawranko@gmail.com>
2018-03-23 23:48:22 -0400

    Initial Commit, setup and auth

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('./handlers/error');
+const authRoutes = require('./routes/auth');
+
+const PORT = process.env.PORT || 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// ROUTES
+app.use('/api/auth', authRoutes);
+
+
+// MIDDLEWARE
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`);
+});
